95f55e02055d49e31c9717295565aa1d9f4aa098
BakhtiyarTayir <BakhtiyarTayir@gmail.com>
2022-08-02 16:37:35 +0500

    added models

--- backend/core/store/admin.py
+++ backend/core/store/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'price', 'created']
+    list_filter = ['category', 'created', 'updated']
+    list_editable = ['price']
+    prepopulated_fields = {'slug': ('title',)}
